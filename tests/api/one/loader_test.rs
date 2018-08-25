51d37f873e8576240d20b8224541963c7c47a862
supaiku <supaiku@protonmail.com>
2018-08-25 20:23:12 +0200

    test: v1 loader

---
+++ tests/api/one/loader_test.rs
@@ -0,0 +1,33 @@
+extern crate arkecosystem_client;
+
+use test_helper::{mock_http_server, mock_client_one, mock_assert_success};
+
+#[test]
+fn test_status() {
+    let _mock = mock_http_server("loader/status");
+    {
+        let client = mock_client_one();
+        let response = client.loader.status();
+        mock_assert_success(&_mock, response);
+    }
+}
+
+#[test]
+fn test_sync() {
+    let _mock = mock_http_server("loader/status/sync");
+    {
+        let client = mock_client_one();
+        let response = client.loader.sync();
+        mock_assert_success(&_mock, response);
+    }
+}
+
+#[test]
+fn test_autoconfigure() {
+    let _mock = mock_http_server("loader/autoconfigure");
+    {
+        let client = mock_client_one();
+        let response = client.loader.autoconfigure();
+        mock_assert_success(&_mock, response);
+    }
+}
--- tests/api/one/mod.rs
+++ tests/api/one/mod.rs
@@ -1,3 +1,4 @@
 pub mod accounts_test;
 pub mod blocks_test;
 pub mod delegates_test;
+pub mod loader_test;
