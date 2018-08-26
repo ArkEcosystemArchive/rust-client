77deb7c4ddcf18ddfcec6d2b1fb77ea0a77d2407
supaiku <supaiku@protonmail.com>
2018-08-26 11:00:22 +0200

    test: add v2 fixtures

--- tests/utils/test_helper.rs
+++ tests/utils/test_helper.rs
@@ -4,6 +4,9 @@ use mockito::{mock, Mock, Matcher};
 use serde_json::{Value};
 use failure;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 const MOCK_HOST: &'static str = "http://127.0.0.1:1234/api/";
 
 pub fn mock_http_request_one(endpoint: &str) -> Mock {
@@ -13,6 +16,22 @@ pub fn mock_http_request_one(endpoint: &str) -> Mock {
         .with_status(200)
         .with_header("content-type", "application/json")
         .with_body(&json!({"success": true}).to_string())
+        .create()
+}
+
+pub fn mock_http_request_two(endpoint: &str) -> Mock {
+    let url = Matcher::Regex(endpoint.to_owned());
+
+    let fixture_name = endpoint.replace("-", "_") + ".json";
+    let mut file = File::open(format!("tests/fixtures/two/{}", fixture_name)).unwrap();
+    let mut response_body = String::new();
+    file.read_to_string(&mut response_body).unwrap();
+
+    println!("{:?}", url);
+    mock("GET", url)
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(&response_body)
         .create()
 }
 
