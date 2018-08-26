35fe7a7bf179e4ebd9826bf7d25a1cc82a860d1c
supaiku <supaiku@protonmail.com>
2018-08-26 20:14:11 +0200

    refactor: cleanupo

--- tests/api/one/signatures_test.rs
+++ tests/api/one/signatures_test.rs
@@ -1,5 +1,3 @@
-extern crate arkecosystem_client;
-
 use test_helper::{mock_http_request_one, mock_client_one, mock_assert_success_one};
 
 #[test]
--- tests/api/two/votes_test.rs
+++ tests/api/two/votes_test.rs
@@ -1,5 +1,3 @@
-extern crate arkecosystem_client;
-
 use test_helper::{mock_http_request_two, mock_client_two, mock_assert_success_two};
 
 #[test]
