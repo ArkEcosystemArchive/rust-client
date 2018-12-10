8058b0b57d28dc1ad432715eb1207aeb5df79244
jaml <dev@jaml.pro>
2018-12-10 11:34:18 +0100

    fix: `Wallet` model/type

--- src/api/two/models.rs
+++ src/api/two/models.rs
@@ -257,6 +257,8 @@ pub struct TransactionTypes {
 pub struct Wallet {
     pub address: String,
     pub public_key: Option<String>,
+    pub username: Option<String>,
+    pub second_public_key: Option<String>,
     #[serde(deserialize_with = "deserialize_u64_as_number_or_string")]
     pub balance: i64,
     pub is_delegate: bool,
