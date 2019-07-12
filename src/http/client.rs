538132319feb81b806a8206c4e3b45f044327459
supaiku <supaiku@protonmail.com>
2019-07-12 22:57:08 +0200

    fix: add request error heuristic

--- src/http/client.rs
+++ src/http/client.rs
@@ -88,22 +88,17 @@ impl Client {
         self.send(builder.body(body?))
     }
 
-    fn send<T: DeserializeOwned>(&self, builder: RequestBuilder) -> Result<T>  {
+    fn send<T: DeserializeOwned>(&self, builder: RequestBuilder) -> Result<T> {
         let response = builder.headers(self.headers.clone()).send()?.text()?;
-        let value: serde_json::Value = from_str(&response)?;
+        let parsed = from_str::<Value>(&response)?;
 
-
-        // Try to deserialize into T. If it fails, assume the API returned
-        // an error.
-        let parsed = from_value::<Response<T>>(value.clone());
-        match parsed {
-            Ok(item) => Ok(item),
-            Err(err) => {
-                println!("{:?}", err);
-
-                // Assume the API returned a RequestError.
-                let request_error = from_value::<RequestError>(value)?;
-                Err(request_error.into())
+        if parsed.is_object() && parsed.as_object().unwrap().contains_key("statusCode") {
+            let request_error = from_value::<RequestError>(parsed)?;
+            Err(request_error.into())
+        } else {
+            match from_value::<Response<T>>(parsed) {
+                Ok(response) => Ok(response),
+                Err(err) => Err(err.into()),
             }
         }
     }
