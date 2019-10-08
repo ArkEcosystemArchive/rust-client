fb721f30088dc7b34c0b7e4b6c5db39fd24e2a3b
Brian Faust <hello@basecode.sh>
2019-10-08 13:28:11 +0300

    wip

--- src/api/models.rs
+++ src/api/models.rs
@@ -300,7 +300,7 @@ fn deserialize_u64_as_number_or_string<'de, D>(de: D) -> Result<u64, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let deser_result: serde_json::Value = try!(Deserialize::deserialize(de));
+    let deser_result: serde_json::Value = Deserialize::deserialize(de)?;
 
     match deser_result {
         serde_json::Value::Number(ref obj) if obj.is_u64() => Ok(obj.as_u64().unwrap()),
